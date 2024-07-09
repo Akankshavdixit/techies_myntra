90bbeef04046e91bd41164044795fbcafd4935f4
Akankshavdixit <131232617+Akankshavdixit@users.noreply.github.com>
2024-07-09 20:19:36 +0530

    done with registration functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import Carousel from "./components/Carousel";
+import CustomerRegistration from "./components/CustomerRegistration";
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 
 
 function App() {
   return (
-    <Carousel/>
+    <Router>
+      
+
+    
+      
+        <Routes>
+
+        <Route path="/carousel" element={<Carousel/>} />
+        <Route path="/customerregistration" element={<CustomerRegistration />} />
+        </Routes>
+
+      
+
+      
+
+
+    </Router>
     
   );
 }
--- server/utils/db/database.js
+++ server/utils/db/database.js
@@ -1,36 +1,4 @@
 
-// const neo4j = require('neo4j-driver');
-
-// exports.connectToDB = async()=> {
-//   const URI = process.env.URL
-//   const USER = 'neo4j'
-//   const PASSWORD = process.env.password
-//   let driver
-
-//   try {
-//     driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD))
-//     const serverInfo = await driver.getServerInfo()
-//     console.log('Connection established')
-//     console.log(serverInfo)
-//   } catch(err) {
-//     console.log(`Connection error\n${err}\nCause: ${err.cause}`)
-//   }
-// };
-
-
-// exports.runQuery = async (query, params) => {
-//   if (!driver) {
-//       throw new Error('Database driver is not initialized. Call connectToDB() first.');
-//   }
-
-//   const session = driver.session();
-//   try {
-//       const result = await session.run(query, params);
-//       return result.records;
-//   } finally {
-//       await session.close();
-//   }
-// };
 
 
 const neo4j = require('neo4j-driver');

ffed3092f2ed649ff568ecb377d92dfd773b0bd6
B Shrinidhi <b.shrinidhi1214@gmail.com>
2024-07-14 11:40:59 +0530

    carousel edited

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,24 +1,23 @@
-
-import React, { useState } from 'react';
+import React from 'react';
 import Slider from 'react-slick';
-import { useSession } from '../context/SessionContext';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import CustomerRegistration from './CustomerRegistration';
-import InfluencerRegistration from './InfluencerRegistration';
 
+const Slide = ({ imageSrc, caption }) => (
+  <div className="mx-auto px-8">
+    <img src={imageSrc} className="h-128 w-full object-cover rounded-lg" alt="Trending style" />
+    <p className="text-center text-gray-600 mt-2">{caption}</p>
+  </div>
+);
 
 function Carousel() {
-
-  // const [showCustomerRegistration, setShowCustomerRegistration] = useState(false);
-  // const [showInfluencerRegistration, setShowInfluencerRegistration] = useState(false);
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
@@ -26,62 +25,70 @@ function Carousel() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
           dots: true
         }
       },
       {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
+        breakpoint: 768,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          initialSlide: 0
         }
       }
     ]
   };
 
-  // const handleCustomerClick = async() => {
-  //   setShowCustomerRegistration(true);
-  //   setShowInfluencerRegistration(false);
-  // }
+  const slides = [
+    {
+      imageSrc: "https://storage.googleapis.com/myntra-weforshe-hackathon.appspot.com/838ddf25-3633-4548-9f00-bdd439b4db681661193870772KASSUALLYRedBrownPrintedJumpsuit6.jpg",
+      caption: "Spring outfits for girls"
+    },
+    {
+      imageSrc: "https://storage.googleapis.com/myntra-weforshe-hackathon.appspot.com/2dab063d-f03b-46a4-beb6-0302a8475c361704262527029RoadsterWomenBaggyFitParachuteJoggers1.jpg",
+      caption: "Casual and trendy"
+    },
+    {
+      imageSrc: "https://storage.googleapis.com/myntra-weforshe-hackathon.appspot.com/78ae2021-8405-4aef-85c8-c4b68b988ae91648366218358TokyoTalkiesWomenBurgundyCropBikerJacket6.jpg",
+      caption: "Explore latest styles"
+    },
+    {
+      imageSrc: "https://storage.googleapis.com/myntra-weforshe-hackathon.appspot.com/aea14200-3a67-4ed1-a659-5929a7ea52a41714359821872SASSAFRASCottonDenimTop1.jpg",
+      caption: "New arrivals"
+    },
+    {
+      imageSrc: "https://storage.googleapis.com/myntra-weforshe-hackathon.appspot.com/527d5c5a-fb7b-43f9-9773-9809a4cb3c331683313624386AthenablueLinenwrapplaysuit1.jpg",
+      caption: "Summer fashion trends"
+    },
+    {
+      imageSrc: "https://storage.googleapis.com/myntra-weforshe-hackathon.appspot.com/d52792d9-8f87-4e80-a916-255e514b9eaf1650970655702-Sangria-Women-Skirts-5161650970655169-1.jpg",
+      caption: "Stylish and comfortable"
+    }
+  ];
 
-  // const handleInfluencerClick = async() => {
-  //   setShowCustomerRegistration(false);
-  //   setShowCustomerRegistration(true);
-  // }
-  
   return (
-    <div >
-    <div className=" mt-10 bg-pink-100 mx-10 py-8">
-      
-      <p className="text-center text-pink-500 font-semibold mt-3">Explore the trending styles</p>
+    <div className="bg-pink-100 mx-10 py-8 rounded-lg shadow-md">
+      <p className="text-center text-pink-500 font-semibold mt-3 text-lg">
+        Explore the Trending Styles
+      </p>
       <div className="mt-7 mx-10">
         <Slider {...settings}>
-          <img src="https://images.bewakoof.com/web/trending-spring-outfits-for-girls-bewakoof-blog-4-1620740562.jpg" className="mx-auto my-4 px-8 h-64 w-full"></img>
-          <img src="https://images.bewakoof.com/web/trending-spring-outfits-for-girls-bewakoof-blog-4-1620740562.jpg" className="mx-auto my-4 h-64 px-8 w-full"></img>
-          <img src="https://images.bewakoof.com/web/trending-spring-outfits-for-girls-bewakoof-blog-4-1620740562.jpg" className="mx-auto my-4 h-64 px-8 w-full"></img>
-          <img src="https://images.bewakoof.com/web/trending-spring-outfits-for-girls-bewakoof-blog-4-1620740562.jpg" className="mx-auto my-4 h-64 px-8 w-full"></img>
-          <img src="https://images.bewakoof.com/web/trending-spring-outfits-for-girls-bewakoof-blog-4-1620740562.jpg" className="mx-auto my-4 h-64 px-8 w-full"></img>
-          <img src="https://images.bewakoof.com/web/trending-spring-outfits-for-girls-bewakoof-blog-4-1620740562.jpg" className="mx-auto my-4 h-64 px-8 w-full"></img>
+          {slides.map((slide, index) => (
+            <Slide key={index} imageSrc={slide.imageSrc} caption={slide.caption} />
+          ))}
         </Slider>
       </div>
-    </div>
-    <div className="mt-16 text-center">
-    <Link to="/customerregistration" className="ml-10 bg-pink-500 p-4">Register as customer</Link>
-    <Link to="/influencerregistration" className="ml-10 bg-orange-500 p-4">Register as influencer</Link>
-    </div>
-
-    
+      <div className="mt-8 flex justify-center">
+        <Link to="/customerregistration" className="mx-4 bg-pink-500 text-white px-6 py-3 rounded-lg hover:bg-pink-600 transition duration-300">
+          Register as Customer
+        </Link>
+        <Link to="/influencerregistration" className="mx-4 bg-orange-500 text-white px-6 py-3 rounded-lg hover:bg-orange-600 transition duration-300">
+          Register as Influencer
+        </Link>
+      </div>
     </div>
   );
 }
